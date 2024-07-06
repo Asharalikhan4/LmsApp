36e111d8b93f06c117afb441a6b60ca740c4f6b0
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-07-06 23:59:01 +0530

    Landing page added

---
+++ frontend/app/(routes)/onboarding/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import OnBoardingScreen from "@/screens/onboarding/onboarding.screen";
+
+const OnBoarding: FC = () => {
+    return (
+        <OnBoardingScreen />
+    );
+};
+
+export default OnBoarding;
