b676b0700285bd3394ef33ace9dd8fb13b2b2ea7
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-07-08 00:20:57 +0530

    register page added

---
+++ frontend/app/(routes)/sign-up/index.tsx
@@ -0,0 +1,10 @@
+import SignUpScreen from "@/screens/auth/signup/signup.screen";
+import { FC } from "react";
+
+const SignUp: FC = () => {
+    return (
+        <SignUpScreen />
+    );
+};
+
+export default SignUp;
