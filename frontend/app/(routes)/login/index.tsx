095eb356a4764d7e7a74502e91ccb340767e8c00
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-07-08 00:02:44 +0530

    Login page added

---
+++ frontend/app/(routes)/login/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import LoginScreen from "@/screens/auth/login/login.screen";
+
+const Login: FC = () => {
+    return (
+        <LoginScreen />
+    );
+};
+
+export default Login;
